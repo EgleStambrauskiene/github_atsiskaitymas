4322908413beaa45a25a34721b638f6a2f4b8709
Egle Stambrauskiene <estambr@yahoo.com>
2018-12-12 01:42:58 +0200

    Dabar header ir footer yra body dalyje

---
+++ scripts/21_baigiamasis-1.js
@@ -0,0 +1,6 @@
+
+$('.headerContainer .navbarRow2 .navbar .collapse .navbar-nav .dropdown .dropdown-menu #ourHorsesLink').click(function(){
+    $('.home').hide();
+}, function(){
+    $('.aboutUsOurHorses').show();
+});
