458981babf37b6381a3fac18e7bb9a26cf576097
Egle Stambrauskiene <estambr@yahoo.com>
2018-12-11 12:58:28 +0200

    Final-11-1
    uzsikrauna gerai (po visu sunaikintu failu atstatymo)

--- scripts/21_baigiamasis.js
+++ scripts/21_baigiamasis.js
@@ -1,1 +1,6 @@
-$('#halymyresModal').modal('handleUpdate');
+
+$('.bodyContainer .topRow .topRowContainer .navbar .navbar-collapse .navbar-nav .nav-item .dropdown-menu #ourHorsesLink').click(function(){
+    $('.bodyContainer .centerRow1').hide();
+}, function(){
+    $('.bodyContainer .centerRow2').show();
+});
