07c119a6216a4d38babd5b04d66258d254f90f94
Egle Stambrauskiene <estambr@yahoo.com>
2018-12-13 20:06:01 +0200

    JQuery linkai veikia :)

--- scripts/21_baigiamasis.js
+++ scripts/21_baigiamasis.js
@@ -1,6 +1,24 @@
 
-$('#ourHorsesLink').click(function(){
-    $('#home').hide();
-}, function(){
-    $('#horses').show();
+$(document).ready(function(){
+    $('#ourHorsesLink').click(function(){
+        $('.everyContainer').hide();
+        $('#horses').show();
+    });
+
+    $('#footOurHorsesLink').click(function(){
+        $('.everyContainer').hide();
+        $('#horses').show();
+    });
+
+    $('#bodyOurHorsesLink').click(function(){
+        $('.everyContainer').hide();
+        $('#horses').show();
+    });
+
+    $('#homeLink').click(function(){
+        $('.everyContainer').hide();
+        $('#home').show();
+    });
 });
+
+
